c60a30ef32d34abe8ebc89a191ffab01ce6e822a
ikwinder0 <ikwinder842@gmail.com>
2022-06-24 08:24:37 +0530

    commit

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -340,8 +340,8 @@ $routes->get('erp/quote-detail/(:segment)', 'Orderquotes::quote_details', ['name
 $routes->get('erp/edit-quote/(:segment)', 'Orderquotes::edit_quote', ['namespace' => 'App\Controllers\Erp','filter' => 'checklogin']);
 $routes->get('erp/print-quote/(:segment)', 'Orderquotes::view_quote_order', ['namespace' => 'App\Controllers\Erp','filter' => 'checklogin']);
 //Agencies
-$routes->get('erp/create-company/', 'Companies::create_agency', ['namespace' => 'App\Controllers\Erp','filter' => 'checklogin']);
-$routes->get('erp/companies-list/', 'Companies::index', ['namespace' => 'App\Controllers\Erp','filter' => 'checklogin']);
+$routes->get('erp/create-company/', 'Companies::create_agency', ['namespace' => 'App\Controllers\Erp','filter' => 'superauth']);
+$routes->get('erp/companies-list/', 'Companies::index', ['namespace' => 'App\Controllers\Erp','filter' => 'superauth']);
 
 
 /**
