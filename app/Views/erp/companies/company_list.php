d2984debdaf8e6ad6f2909f2149db54fb29d1f20
ikwinder0 <ikwinder842@gmail.com>
2022-06-24 14:50:11 +0530

    commit

--- app/Views/erp/companies/company_list.php
+++ app/Views/erp/companies/company_list.php
@@ -64,33 +64,6 @@ $membership_plans = $MembershipModel->orderBy('membership_id', 'ASC')->findAll()
                   </div>
                 </div>
               </div>
-              <hr class="m-0 mb-3">
-              <span class="preview-title-lg">
-              <?= lang('Membership.xin_membership_plan');?>
-              </span>
-              <div class="row">
-                <div class="col-md-6">
-                  <div class="form-group">
-                    <label for="membership_type">
-                      <?= lang('Membership.xin_membership_type');?>
-                      <span class="text-danger">*</span> </label>
-                    <select class="form-control form-select" name="membership_type" data-plugin="select_hrm" data-placeholder="<?= lang('Membership.xin_membership_type');?>">
-                      <option value="">
-                      <?= lang('Main.xin_select_one');?>
-                      </option>
-                      <?php foreach($membership_plans as $plans) {?>
-                      <option value="<?= $plans['membership_id'];?>">
-                      <?= $plans['membership_type'];?>
-                      </option>
-                      <?php } ?>
-                    </select>
-                  </div>
-                </div>
-              </div>
-              <hr class="m-0 mb-3">
-              <span class="preview-title-lg">
-              <?= lang('Main.xin_employee_other_info');?>
-              </span>
               <div class="row">
                 <div class="col-md-6">
                   <div class="form-group">
@@ -125,6 +98,7 @@ $membership_plans = $MembershipModel->orderBy('membership_id', 'ASC')->findAll()
                   </div>
                 </div>
               </div>
+			  <hr class="m-0 mb-3">
             </div>
             <div class="card-footer text-right">
               <button type="reset" class="btn btn-light" href="#add_form" data-toggle="collapse" aria-expanded="false">
