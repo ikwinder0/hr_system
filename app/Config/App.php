37ec5c83237cf6d419608fc532e0b73e96b0a895
ikwinder0 <ikwinder842@gmail.com>
2022-06-27 07:38:15 +0530

    commit

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
 	| environments.
 	|
 	*/
-	public $baseURL = 'http://thecodehub-tech.preview-domain.com/';
+	public $baseURL = 'http://thecodehub.tech/';
 
 	/*
 	|--------------------------------------------------------------------------
