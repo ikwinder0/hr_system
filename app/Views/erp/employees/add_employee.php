2a3fbcd90d0c6d4ed5bf2c7c3e795355f3ff80a4
ikwinder0 <ikwinder842@gmail.com>
2022-06-28 10:16:27 +0530

    commits

--- app/Views/erp/employees/add_employee.php
+++ app/Views/erp/employees/add_employee.php
@@ -85,3 +85,218 @@ $get_animate='';
     </ul>
 </div>
 <hr class="border-light m-0 mb-3">
+<div id="accordion">
+    <div
+        id="add_form"
+        class="collapse add-form <?php echo $get_animate;?>"
+        data-parent="#accordion"
+        style="">
+        <?php $attributes = array('name' => 'add_employee', 'id' => 'xin-form', 'autocomplete' => 'off');?>
+        <?php $hidden = array('user_id' => 0);?>
+        <?= form_open_multipart('erp/employees/add_employee', $attributes, $hidden);?>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card mb-2">
+                    <div class="card-header">
+                        <h5>
+                            <?= lang('Main.xin_add_new');?>
+                            <?= lang('Dashboard.dashboard_employee');?>
+                        </h5>
+                        <div class="card-header-right">
+                            <a
+                                data-toggle="collapse"
+                                href="#add_form"
+                                aria-expanded="false"
+                                class="collapsed btn btn-sm waves-effect waves-light btn-primary m-0">
+                                <i data-feather="minus"></i>
+                                <?= lang('Main.xin_hide');?>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <label for="logo">
+                                    <?= lang('Main.xin_e_details_profile_picture');?>
+                                    <span class="text-danger">*</span>
+                                </label>
+                                <div class="custom-file">
+                                    <input type="file" class="custom-file-input" name="file">
+                                    <label class="custom-file-label">
+                                        <?= lang('Main.xin_choose_file');?>
+                                    </label>
+                                    <small>
+                                        <?= lang('Main.xin_company_file_type');?>
+                                    </small>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label for="company_name">
+                                        <?= lang('Main.xin_employee_first_name');?>
+                                        <span class="text-danger">*</span>
+                                    </label>
+                                    <div class="input-group">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text">
+                                                <i class="fas fa-user"></i></span></div>
+                                        <input
+                                            type="text"
+                                            class="form-control"
+                                            placeholder="<?= lang('Main.xin_employee_first_name');?>"
+                                            name="first_name">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label for="last_name" class="control-label">
+                                        <?= lang('Main.xin_employee_last_name');?>
+                                        <span class="text-danger">*</span></label>
+                                    <div class="input-group">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text">
+                                                <i class="fas fa-user"></i></span></div>
+                                        <input
+                                            type="text"
+                                            class="form-control"
+                                            placeholder="<?= lang('Main.xin_employee_last_name');?>"
+                                            name="last_name">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label for="email">
+                                        <?= lang('Main.xin_email');?>
+                                        <span class="text-danger">*</span>
+                                    </label>
+                                    <div class="input-group">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text">
+                                                <i class="fas fa-envelope"></i></span></div>
+                                        <input
+                                            class="form-control"
+                                            placeholder="<?= lang('Main.xin_email');?>"
+                                            name="email"
+                                            type="text">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label for="contact_number">
+                                        <?= lang('Main.xin_contact_number');?>
+                                        <span class="text-danger">*</span></label>
+                                    <div class="input-group">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text">
+                                                <i class="fas fa-phone"></i></span></div>
+                                        <input
+                                            class="form-control"
+                                            placeholder="<?= lang('Main.xin_contact_number');?>"
+                                            name="contact_number"
+                                            type="text">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label for="contact_number">
+                                        <?= lang('Main.xin_dob');?>
+                                        <span class="text-danger">*</span></label>
+                                    <div class="input-group">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text">
+                                                <i class="fas fa-birthday-cake"></i></span></div>
+                                        <input
+                                            class="form-control maxdate"
+                                            placeholder="<?= lang('Main.xin_dob');?>"
+                                            name="dob"
+                                            type="text">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label for="gender" class="control-label">
+                                        <?= lang('Main.xin_employee_gender');?>
+                                    </label>
+                                    <select
+                                        class="form-control"
+                                        name="gender"
+                                        data-plugin="select_hrm"
+                                        data-placeholder="<?= lang('Main.xin_employee_gender');?>">
+                                        <option value="1">
+                                            <?= lang('Main.xin_gender_male');?>
+                                        </option>
+                                        <option value="2">
+                                            <?= lang('Main.xin_gender_female');?>
+                                        </option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label for="contact_number">
+                                        <?= lang('Main.xin_nationality');?>
+                                        <span class="text-danger">*</span></label>
+                                    <div class="input-group">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text">
+                                                <i class="fas fa-flag"></i></span></div>
+                                        <input
+                                            class="form-control"
+                                            placeholder="<?= lang('Main.xin_nationality');?>"
+                                            name="nationality"
+                                            type="text">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label for="contact_number">
+                                        <?= lang('Main.xin_marital_status');?>
+                                        <span class="text-danger">*</span></label>
+                                    <select
+                                        class="form-control"
+                                        name="marital_status"
+                                        data-plugin="select_hrm"
+                                        data-placeholder="<?= lang('Main.xin_employee_gender');?>">
+                                        <option value="1">
+                                            <?= lang('Main.xin_marital_single');?>
+                                        </option>
+                                        <option value="2">
+                                            <?= lang('Main.xin_marital_married');?>
+                                        </option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label for="contact_number">
+                                        <?= lang('Main.xin_religion');?>
+                                        <span class="text-danger">*</span></label>
+                                    <div class="input-group">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text">
+                                                <i class="fas fa-atom"></i></span></div>
+                                        <input
+                                            class="form-control"
+                                            placeholder="<?= lang('Main.xin_religion');?>"
+                                            name="religion"
+                                            type="text">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<?= form_close(); ?>
+	</div>
+</div>
+</div>
+</div>
+</div>
