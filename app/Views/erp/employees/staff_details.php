6832b2194fb311ec8f491bdb88760aedffc60b0d
ikwinder0 <ikwinder842@gmail.com>
2022-06-29 17:24:28 +0530

    commit

--- app/Views/erp/employees/staff_details.php
+++ app/Views/erp/employees/staff_details.php
@@ -425,21 +425,21 @@ $cmodule_attributes = $Moduleattributes->where('company_id',$company_id)->where(
 					<div class="box-body">
 						<div class="row bs-wizard" style="border-bottom:0;">
                 
-							<div class="col-md-3 bs-wizard-step /*complete*/">
+							<div class="col-md-3 bs-wizard-step disabled /*complete*/">
 							  <div class="text-center bs-wizard-stepnum">Pre-Screening</div>
 							  <div class="progress"><div class="progress-bar"></div></div>
 							  <a href="#" class="bs-wizard-dot"></a>
 							  <div class="bs-wizard-info text-center">Lorem ipsum dolor sit amet.</div>
 							</div>
 							
-							<div class="col-md-3 bs-wizard-step /*complete*/"><!-- complete -->
+							<div class="col-md-3 bs-wizard-step disabled /*complete*/"><!-- complete -->
 							  <div class="text-center bs-wizard-stepnum">Interview</div>
 							  <div class="progress"><div class="progress-bar"></div></div>
 							  <a href="#" class="bs-wizard-dot"></a>
 							  <div class="bs-wizard-info text-center">Nam mollis tristique erat vel tristique. Aliquam erat volutpat. Mauris et vestibulum nisi. Duis molestie nisl sed scelerisque vestibulum. Nam placerat tristique placerat</div>
 							</div>
 							
-							<div class="col-md-3 bs-wizard-step /*active*/"><!-- complete -->
+							<div class="col-md-3 bs-wizard-step disabled /*active*/"><!-- complete -->
 							  <div class="text-center bs-wizard-stepnum">Feedback</div>
 							  <div class="progress"><div class="progress-bar"></div></div>
 							  <a href="#" class="bs-wizard-dot"></a>
