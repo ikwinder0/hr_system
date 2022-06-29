2163926130696b31b962cee4fd2484fc68539a45
ikwinder0 <ikwinder842@gmail.com>
2022-06-29 17:07:57 +0530

    commit

--- app/Views/erp/employees/staff_details.php
+++ app/Views/erp/employees/staff_details.php
@@ -425,28 +425,28 @@ $cmodule_attributes = $Moduleattributes->where('company_id',$company_id)->where(
 					<div class="box-body">
 						<div class="row bs-wizard" style="border-bottom:0;">
                 
-							<div class="col-xs-3 bs-wizard-step complete">
+							<div class="col-md-3 bs-wizard-step complete">
 							  <div class="text-center bs-wizard-stepnum">Step 1</div>
 							  <div class="progress"><div class="progress-bar"></div></div>
 							  <a href="#" class="bs-wizard-dot"></a>
 							  <div class="bs-wizard-info text-center">Lorem ipsum dolor sit amet.</div>
 							</div>
 							
-							<div class="col-xs-3 bs-wizard-step complete"><!-- complete -->
+							<div class="col-md-3 bs-wizard-step complete"><!-- complete -->
 							  <div class="text-center bs-wizard-stepnum">Step 2</div>
 							  <div class="progress"><div class="progress-bar"></div></div>
 							  <a href="#" class="bs-wizard-dot"></a>
 							  <div class="bs-wizard-info text-center">Nam mollis tristique erat vel tristique. Aliquam erat volutpat. Mauris et vestibulum nisi. Duis molestie nisl sed scelerisque vestibulum. Nam placerat tristique placerat</div>
 							</div>
 							
-							<div class="col-xs-3 bs-wizard-step active"><!-- complete -->
+							<div class="col-md-3 bs-wizard-step active"><!-- complete -->
 							  <div class="text-center bs-wizard-stepnum">Step 3</div>
 							  <div class="progress"><div class="progress-bar"></div></div>
 							  <a href="#" class="bs-wizard-dot"></a>
 							  <div class="bs-wizard-info text-center">Integer semper dolor ac auctor rutrum. Duis porta ipsum vitae mi bibendum bibendum</div>
 							</div>
 							
-							<div class="col-xs-3 bs-wizard-step disabled"><!-- active -->
+							<div class="col-md-3 bs-wizard-step disabled"><!-- active -->
 							  <div class="text-center bs-wizard-stepnum">Step 4</div>
 							  <div class="progress"><div class="progress-bar"></div></div>
 							  <a href="#" class="bs-wizard-dot"></a>
