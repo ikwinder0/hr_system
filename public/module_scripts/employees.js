a285cc4e67554add36a9a49cee3cfe6e2ab68596
ikwinder0 <ikwinder842@gmail.com>
2022-07-01 21:10:37 +0530

    commit

--- public/module_scripts/employees.js
+++ public/module_scripts/employees.js
@@ -188,7 +188,7 @@ var v = $(".add_employee_form").validate({
         },
         file: {
           required: true,
-		  accept: "image/jpeg, image/jpg, image/png"
+		  extension: "jpg|png"
         },
         first_name: {
           required: true,
