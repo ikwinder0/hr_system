536fd81abf12205e3e990b8e4106f09348cde9a7
ikwinder0 <ikwinder842@gmail.com>
2022-07-06 16:49:21 +0530

    commit

--- app/Controllers/Erp/Employees.php
+++ app/Controllers/Erp/Employees.php
@@ -54,7 +54,7 @@ class Employees extends BaseController {
 			$session->setFlashdata('err_not_logged_in',lang('Dashboard.err_not_logged_in'));
 			return redirect()->to(site_url('erp/login'));
 		}
-		if($user_info['user_type'] != 'company'){
+		if($user_info['user_type'] == 'staff'){
 			$session->setFlashdata('unauthorized_module',lang('Dashboard.xin_error_unauthorized_module'));
 			return redirect()->to(site_url('erp/desk'));
 		}
