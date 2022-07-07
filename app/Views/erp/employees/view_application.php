98722f76181b65cb93462877160ef7d7d0bec0cb
ikwinder0 <ikwinder842@gmail.com>
2022-07-07 09:32:06 +0530

    commit

--- app/Views/erp/employees/view_application.php
+++ app/Views/erp/employees/view_application.php
@@ -86,9 +86,8 @@ $cmodule_attributes = '';
         <div class="float-right">
           <?= $_status?>
         </div>
-        <div class="media user-about-block align-items-center mt-0 mb-3">
-          <div class="position-relative d-inline-block"> <img class="img-radius img-fluid wid-80" src="<?= base_url().'/public/uploads/users/'.$result['profile_photo'];?>" alt="<?= $result['first_name'].' '.$result['last_name']; ?>">
-            <div class="certificated-badge"> <i class="fas fa-certificate text-primary bg-icon"></i> <i class="fas fa-check front-icon text-white"></i> </div>
+        <div class="user-about-block align-items-center mt-0 mb-3">
+          <div class="position-relative d-inline-block"> <img class="img-fluid wid-80" src="<?= base_url().'/public/uploads/users/'.$result['profile_photo'];?>" alt="<?= $result['first_name'].' '.$result['last_name']; ?>">
           </div>
           <div class="media-body ml-3">
             <h6 class="mb-1">
