7bc986c3d2f543fbd64775b7dc5bde6181ce0fd7
ikwinder0 <ikwinder842@gmail.com>
2022-07-07 11:38:11 +0530

    commit

--- app/Views/erp/employees/view_application.php
+++ app/Views/erp/employees/view_application.php
@@ -214,6 +214,7 @@ $cmodule_attributes = '';
 						</p>
 					</div>
 				</div>
+				<hr class="">
 				<div class="row">
 					<div class="col-md-6">
                         <h6 class="mb-0"><?= lang('Employees.xin_ethnicity_type_title');?></h6>
@@ -244,233 +245,56 @@ $cmodule_attributes = '';
 						</p>
 					</div>
 				</div>
-				
-                <div class="row">
-                  <div class="col-md-6">
-                    <div class="form-group">
-                      <label for="date_of_birth">
-                        <?= lang('Employees.xin_employee_dob');?>
-                      </label>
-                      <div class="input-group">
-                        <input class="form-control date" placeholder="<?= lang('Employees.xin_employee_dob');?>" name="date_of_birth" type="text" value="<?= $employee_detail['date_of_birth'];?>">
-                        <div class="input-group-append"><span class="input-group-text"><i class="fas fa-calendar-alt"></i></span></div>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="col-md-6">
-                    <div class="form-group">
-                      <label for="marital_status" class="control-label">
-                        <?= lang('Employees.xin_employee_mstatus');?>
-                      </label>
-                      <select class="form-control" name="marital_status" data-plugin="select_hrm" data-placeholder="<?= lang('Employees.xin_employee_mstatus');?>">
-                        <option value="0" <?php if($employee_detail['marital_status']==0):?> selected <?php endif; ?>>
-                        <?= lang('Employees.xin_status_single');?>
-                        </option>
-                        <option value="1" <?php if($employee_detail['marital_status']==1):?> selected <?php endif; ?>>
-                        <?= lang('Employees.xin_status_married');?>
-                        </option>
-                        <option value="2" <?php if($employee_detail['marital_status']==2):?> selected <?php endif; ?>>
-                        <?= lang('Employees.xin_status_widowed');?>
-                        </option>
-                        <option value="3" <?php if($employee_detail['marital_status']==3):?> selected <?php endif; ?>>
-                        <?= lang('Employees.xin_status_divorced_separated');?>
-                        </option>
-                      </select>
-                    </div>
-                  </div>
-                </div>
-                <div class="row">
-                  <div class="col-md-4">
-                    <div class="form-group">
-                      <label for="estate">
-                        <?= lang('Main.xin_state');?>
-                      </label>
-                      <input class="form-control" placeholder="<?= lang('Main.xin_state');?>" name="state" type="text" value="<?= $result['state'];?>">
-                    </div>
-                  </div>
-                  <div class="col-md-4">
-                    <div class="form-group">
-                      <label for="ecity">
-                        <?= lang('Main.xin_city');?>
-                      </label>
-                      <input class="form-control" placeholder="<?= lang('Main.xin_city');?>" name="city" type="text" value="<?= $result['city'];?>">
-                    </div>
-                  </div>
-                  <div class="col-md-4">
-                    <div class="form-group">
-                      <label for="ezipcode" class="control-label">
-                        <?= lang('Main.xin_zipcode');?>
-                      </label>
-                      <input class="form-control" placeholder="<?= lang('Main.xin_zipcode');?>" name="zipcode" type="text" value="<?= $result['zipcode'];?>">
-                    </div>
-                  </div>
-                  <div class="col-md-6">
-                    <div class="form-group">
-                      <label for="email" class="control-label">
-                        <?= lang('Employees.xin_ethnicity_type_title');?>
-                      </label>
-                      <select class="form-control" name="religion" data-plugin="select_hrm" data-placeholder="<?= lang('Employees.xin_ethnicity_type_title');?>">
-                        <option value=""></option>
-                        <?php foreach($religion as $ireligion) {?>
-                        <option value="<?= $ireligion['constants_id']?>" <?php if($ireligion['constants_id']==$employee_detail['religion_id']):?> selected="selected"<?php endif;?>>
-                        <?= $ireligion['category_name']?>
-                        </option>
-                        <?php } ?>
-                      </select>
-                    </div>
-                  </div>
-             
-                  <div class="col-md-6">
-                    <div class="form-group">
-                      <label for="nationality_id">
-                        <?= lang('Employees.xin_nationality');?>
-                      </label>
-                      <select class="form-control" name="country" data-plugin="select_hrm" data-placeholder="<?= lang('Employees.xin_nationality');?>">
-                        <option value="">
-                        <?= lang('Main.xin_select_one');?>
-                        </option>
-                        <?php foreach($all_countries as $country) {?>
-                        <option value="<?= $country['country_id'];?>" <?php if($country['country_id'] == $result['country']):?> selected="selected"<?php endif;?>>
-                        <?= $country['country_name'];?>
-                        </option>
-                        <?php } ?>
-                      </select>
-                    </div>
-                  </div>
-                </div>
-                <?php if($bcount_module_attributes > 0):?>
-                <div class="row">
-                  <?php foreach($bmodule_attributes as $mattribute):?>
-                  <?php $attribute_info = $Moduleattributesval->where('user_id',$user_id)->where('module_attributes_id', $mattribute['custom_field_id'])->first();?>
-                  <?php
-						if($attribute_info){
-							$attr_val = $attribute_info['attribute_value'];
-						} else {
-							$attr_val = '';
-						}
-					?>
-                  <?php if($mattribute['attribute_type'] == 'date'){?>
-                  <?php
-				  	if($mattribute['validation']==1):
-						$validate_opt = '<span class="text-danger">*</span>';
-					else:
-						$validate_opt = '';
-					endif;
-				  ?>
-                  <div class="<?= $mattribute['col_width'];?>">
-                    <div class="form-group">
-                      <label for="<?php echo $mattribute['attribute'];?>"><?php echo $mattribute['attribute_label'];?> <?= $validate_opt;?></label>
-                      <input class="form-control date" 
-                      placeholder="<?php echo $mattribute['attribute_label'];?>" name="<?php echo $mattribute['attribute'];?>" type="text" value="<?= $attr_val;?>">
-                    </div>
-                  </div>
-                  <?php } else if($mattribute['attribute_type'] == 'select'){?>
-                  <?php $iselc_val = $Moduleattributesvalsel->where('custom_field_id', $mattribute['custom_field_id'])->first();?>
-                  <?php
-				  	if($mattribute['validation']==1):
-						$validate_opt = '<span class="text-danger">*</span>';
-					else:
-						$validate_opt = '';
-					endif;
-				  ?>
-                  <div class="<?= $mattribute['col_width'];?>">
-                    <?php $iselc_val = $Moduleattributesvalsel->where('custom_field_id',$mattribute['custom_field_id'])->findAll();?>
-                    <div class="form-group">
-                      <label for="<?php echo $mattribute['attribute'];?>"><?php echo $mattribute['attribute_label'];?> <?= $validate_opt;?></label>
-                      <select class="form-control" name="<?php echo $mattribute['attribute'];?>" data-plugin="select_hrm" data-placeholder="<?php echo $mattribute['attribute_label'];?>">
-                        <?php foreach($iselc_val as $selc_val) {?>
-                        <option value="<?php echo $selc_val['attributes_select_value_id']?>" <?php if($attr_val==$selc_val['attributes_select_value_id']):?> selected="selected"<?php endif;?>><?php echo $selc_val['select_label']?></option>
-                        <?php } ?>
-                      </select>
-                    </div>
-                  </div>
-                  <?php } else if($mattribute['attribute_type'] == 'textarea'){?>
-                  <?php
-				  	if($mattribute['validation']==1):
-						$validate_opt = '<span class="text-danger">*</span>';
-					else:
-						$validate_opt = '';
-					endif;
-				  ?>
-                  <div class="<?= $mattribute['col_width'];?>">
-                    <div class="form-group">
-                      <label for="<?php echo $mattribute['attribute'];?>"><?php echo $mattribute['attribute_label'];?> <?= $validate_opt;?></label>
-                      <textarea class="form-control" placeholder="<?php echo $mattribute['attribute_label'];?>" name="<?php echo $mattribute['attribute'];?>" type="text"><?= $attr_val;?></textarea>
-                    </div>
-                  </div>
-                  <?php } else { ?>
-                  <?php
-				  	if($mattribute['validation']==1):
-						$validate_opt = '<span class="text-danger">*</span>';
-					else:
-						$validate_opt = '';
-					endif;
-				  ?>
-                  <div class="<?= $mattribute['col_width'];?>">
-                    <div class="form-group">
-                      <label for="<?php echo $mattribute['attribute'];?>"><?php echo $mattribute['attribute_label'];?> <?= $validate_opt;?></label>
-                      <input class="form-control" placeholder="<?php echo $mattribute['attribute_label'];?>" name="<?php echo $mattribute['attribute'];?>" type="text" value="<?= $attr_val;?>">
-                    </div>
-                  </div>
-                  <?php }	?>
-                  <?php endforeach;?>
-                </div>
-                <?php endif;?>
+				<hr class="">
+				<div class="row">
+					<div class="col-md-6">
+                        <h6 class="mb-0"><?= lang('Main.xin_city');?></h6>
+					</div>
+					<div class="col-md-6">
+					    <p class="mb-0 text-muted">
+							<?= $result['city'];?>
+						</p>
+					</div>
+				</div>
+				<hr class="">
+				<div class="row">
+					<div class="col-md-6">
+                        <h6 class="mb-0"><?= lang('Main.xin_zipcode');?></h6>
+					</div>
+					<div class="col-md-6">
+					    <p class="mb-0 text-muted">
+							<?= $result['zipcode'];?>
+						</p>
+					</div>
+				</div>
+				<hr class="">
+				<div class="row">
+					<div class="col-md-6">
+                        <h6 class="mb-0"><?= lang('Employees.xin_nationality');?></h6>
+					</div>
+					<div class="col-md-6">
+					    <p class="mb-0 text-muted">
+							<?php foreach($all_countries as $country) 
+							if($country['country_id'] == $result['country']):?> 
+							
+							<?= $country['country_name'];?>
+                        <?php endif;
+                         } ?>
+						</p>
+					</div>
+				</div>
+                
+               
               </div>
             </div>
           </div>
           <div class="card-footer text-right">
-            <button  type="submit" class="btn btn-primary">
-            <?= lang('Employees.xin_update_profile');?>
-            </button>
+            
           </div>
-          <?= form_close(); ?>
+         
         </div>
       </div>
-     
-      <?php if(in_array('staff4',staff_role_resource()) || $user_info['user_type'] == 'company') { ?>
-      <div class="tab-pane fade" id="user-set-picture" role="tabpanel" aria-labelledby="user-set-picture-tab">
-        <div class="card">
-          <div class="card-header">
-            <h5><i data-feather="image" class="icon-svg-primary wid-20"></i><span class="p-l-5">
-              <?= lang('Main.xin_e_details_profile_picture');?>
-              </span></h5>
-          </div>
-          <div class="card-body pb-2">
-            <div class="box-body">
-              <?php $attributes = array('name' => 'edit_user_photo', 'id' => 'edit_user_photo', 'autocomplete' => 'off');?>
-              <?php $hidden = array('token' => $segment_id);?>
-              <?= form_open('erp/employees/update_profile_photo', $attributes, $hidden);?>
-              <div class="form-body">
-                <div class="row">
-                  <div class="col-md-12">
-                    <div class="form-group">
-                      <label for="logo">
-                        <?= lang('Main.xin_e_details_profile_picture');?>
-                        <span class="text-danger">*</span> </label>
-                      <div class="custom-file">
-                        <input type="file" class="custom-file-input" name="file">
-                        <label class="custom-file-label">
-                          <?= lang('Main.xin_choose_file');?>
-                        </label>
-                        <small>
-                        <?= lang('Main.xin_company_file_type');?>
-                        </small> </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="card-footer text-right">
-            <button  type="submit" class="btn btn-primary">
-            <?= lang('Employees.xin_update_pic');?>
-            </button>
-          </div>
-          <?= form_close(); ?>
-        </div>
-      </div>
-      <?php }?>
+      
 	  <div class="tab-pane fade" id="user-application-status" role="tabpanel" aria-labelledby="user-application-status-tab">
         <div class="card">
 			<div class="card-header">
