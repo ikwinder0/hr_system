255553f76506ca6cdf95edff81af03d9d1d19c7f
ikwinder0 <ikwinder842@gmail.com>
2022-07-07 16:27:18 +0530

    commit

--- app/Controllers/Erp/Employees.php
+++ app/Controllers/Erp/Employees.php
@@ -207,7 +207,7 @@ class Employees extends BaseController {
 		
 		//change application status to in progress
 		
-		if($iuser_id->is_active == '2'){
+		if($user_val->is_active == '2'){
 			$UsersModel->update(['is_active' => 3]);
 		}
 		
