835b7ac516d6c4d7cb97a2504f8c204d20b81dd1
ikwinder0 <ikwinder842@gmail.com>
2022-07-15 18:48:04 +0530

    commits

--- app/Controllers/Erp/Employees.php
+++ app/Controllers/Erp/Employees.php
@@ -1052,13 +1052,13 @@ class Employees extends BaseController {
 				'contact_address' => '',
 				'created_at' => date('d-m-Y h:i:s')
 			];
-			
+			$StaffdetailsModel->insert($data2);
 			$data3 = [
 				'candidate_id' => $user_id,
 				'company_id' => $company_id,
-				'designation_id' => $designation_id	
+				'designation_id' => $applied_for	
 			];
-			$StaffdetailsModel->insert($data2);
+			
 			$JobcandidatesModel->insert($data3);
 			
 			$Return['csrf_hash'] = csrf_hash();	
