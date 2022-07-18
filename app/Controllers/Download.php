8f1b5ec1c1068a339c67adfc96e11b1b51eed030
ikwinder0 <ikwinder842@gmail.com>
2022-07-18 11:56:57 +0530

    commit

--- app/Controllers/Download.php
+++ app/Controllers/Download.php
@@ -88,7 +88,7 @@ class Download extends BaseController {
 			if($type=="candidate_documents"){
 				$user = $this->request->getGet('user');
 				print_r(base_url().'/public/'.$type.'/'.$user.'/'.udecode($this->request->getGet('filename')));
-				die;
+				die; 
 				$data = file_get_contents(base_url().'/public/uploads/'.$type.'/'.$user.'/'.udecode($this->request->getGet('filename')));
 			}else{
 				$data = file_get_contents(base_url().'/public/uploads/'.$type.'/'.udecode($this->request->getGet('filename')));
