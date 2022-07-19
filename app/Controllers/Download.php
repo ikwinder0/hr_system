8188db2e8379fdfc8f4294a2330611b0e7607f3a
ikwinder0 <ikwinder842@gmail.com>
2022-07-19 12:45:18 +0530

    commit

--- app/Controllers/Download.php
+++ app/Controllers/Download.php
@@ -83,12 +83,11 @@ class Download extends BaseController {
 
         if (file_exists($file)) {
             header('Content-Description: File Transfer');
-            header('Content-Type: application/octet-stream');
+            header('Content-Type: application/pdf');
             header('Content-Disposition: attachment; filename='.basename($file));
             header('Content-Transfer-Encoding: binary');
             header('Expires: 0');
             header('Cache-Control: must-revalidate');
-            header('Pragma: public');
             header('Content-Length: ' . filesize($file));
             ob_clean();
             flush();
