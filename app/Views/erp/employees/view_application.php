e954dbcc85812e9e017aaecb072194afd24e1550
ikwinder0 <ikwinder842@gmail.com>
2022-07-21 20:54:00 +0530

    commit

--- app/Views/erp/employees/view_application.php
+++ app/Views/erp/employees/view_application.php
@@ -452,80 +452,42 @@ $interview = $JobinterviewsModel->where('candidate_id', $result['user_id'])->fir
 						</div>
 						<?php endif; ?>
 						
-						<!-- Vertical Steppers -->
-<div class="row mt-1">
-  <div class="col-md-12">
-
-    <!-- Stepers Wrapper -->
-    <ul class="stepper stepper-vertical">
-
-      <!-- First Step -->
-      <li class="completed">
-        <a href="#!">
-          <span class="circle">1</span>
-          <span class="label">First step</span>
-        </a>
-      </li>
-
-      <!-- Second Step -->
-      <li class="active">
-
-        <!--Section Title -->
-        <a href="#!">
-          <span class="circle">2</span>
-          <span class="label">Second step</span>
-        </a>
-
-        <!-- Section Description -->
-        <div class="step-content grey lighten-3">
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse cupiditate voluptate facere
-            iusto
-            quaerat
-            vitae excepturi, accusantium ut aliquam repellat atque nesciunt nostrum similique. Inventore
-            nostrum
-            ut,
-            nobis porro sapiente.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore error excepturi veniam nemo
-            repellendus, distinctio soluta vitae at sit saepe. Optio eaque quia excepturi adipisci pariatur
-            totam,
-            atque odit fugiat.</p>
-          <p>Deserunt voluptatem illum quae nisi soluta eum perferendis nesciunt asperiores tempore saepe
-            reiciendis,
-            vero quod a dolor corporis natus qui magni quas fuga rem excepturi laboriosam. Quisquam
-            expedita ab
-            fugiat.</p>
-        </div>
-      </li>
-
-      <!-- Third Step -->
-      <li class="warning">
-        <a href="#!">
-          <span class="circle"><i class="fas fa-exclamation"></i></span>
-          <span class="label">Third step</span>
-        </a>
-      </li>
-
-      <!-- Fourth Step -->
-      <li>
-        <a href="#!">
-          <span class="circle">4</span>
-          <span class="label">Fourth step</span>
-        </a>
-      </li>
-
-    </ul>
-    <!-- /.Stepers Wrapper -->
-
-  </div>
-</div>
-
-<!-- Steppers Navigation -->
-<div class="row mt-1">
-  <div class="col-md-12 text-right">
-    <button class="btn btn-flat btn-sm">Cancel</button>
-    <button class="btn btn-primary btn-sm">Next</button>
-  </div>
-</div>
+						<div class="row">
+							<div class="col-md-12">
+								<div class="stepper d-flex flex-column mt-5 ml-2">
+									<div class="d-flex mb-1">
+									  <div class="d-flex flex-column pr-4 align-items-center">
+										<div class="rounded-circle py-2 px-3 bg-primary text-white mb-1">1</div>
+										<div class="line h-100"></div>
+									  </div>
+									  <div>
+										<h5 class="text-dark">Create your application respository</h5>
+										<p class="lead text-muted pb-3">Choose your website name & create repository</p>
+									  </div>
+									</div>
+									<div class="d-flex mb-1">
+									  <div class="d-flex flex-column pr-4 align-items-center">
+										<div class="rounded-circle py-2 px-3 bg-primary text-white mb-1">2</div>
+										<div class="line h-100"></div>
+									  </div>
+									  <div>
+										<h5 class="text-dark">Clone application respository</h5>
+										<p class="lead text-muted pb-3">Go to your dashboard and clone Git respository from the url in the dashboard of your application</p>
+									  </div>
+									</div>
+									<div class="d-flex mb-1">
+									  <div class="d-flex flex-column pr-4 align-items-center">
+										<div class="rounded-circle py-2 px-3 bg-primary text-white mb-1">3</div>
+										<div class="line h-100 d-none"></div>
+									  </div>
+									  <div>
+										<h5 class="text-dark">Make changes and push!</h5>
+										<p class="lead text-muted pb-3">Now make changes to your application source code, test it then commit &amp; push</p>
+									  </div>
+									</div>
+								  </div>
+							</div>
+						</div>
 						
 					</div>
 				</div>
