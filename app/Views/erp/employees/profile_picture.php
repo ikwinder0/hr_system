70ac4b2e7ebc9870fefe2d4005a11f23b08711da
ikwinder0 <ikwinder842@gmail.com>
2022-08-09 15:38:49 +0530

    changes

--- app/Views/erp/employees/profile_picture.php
+++ app/Views/erp/employees/profile_picture.php
@@ -115,26 +115,44 @@ $interview = $JobinterviewsModel->where('candidate_id', $result['user_id'])->fir
   <div class="col-lg-8">
     <div class="tab-content" id="user-set-tabContent">
         <div class="tab-pane fade show active" id="user-set-basicinfo" role="tabpanel" aria-labelledby="user-set-basicinfo-tab">
-        <div class="card user-profile-list">
+        <div class="card">
           <div class="card-header">
-            <h5><i data-feather="file-plus" class="icon-svg-primary wid-20"></i><span class="p-l-5">
-              <?= lang('Employees.xin_documents');?>
+            <h5><i data-feather="image" class="icon-svg-primary wid-20"></i><span class="p-l-5">
+              <?= lang('Main.xin_e_details_profile_picture');?>
               </span></h5>
           </div>
-          <div class="card-body">
-            <div class="box-datatable table-responsive">
-              <table class="table table-striped table-bordered dataTable" id="xin_table_document" style="width:100%;">
-                <thead>
-                  <tr>
-                    <th><?= lang('Employees.xin_document_name');?></th>
-                    <th><?= lang('Employees.xin_document_type');?></th>
-                    <th><?= lang('Employees.xin_document_file');?></th>
-                  </tr>
-                </thead>
-              </table>
+          <div class="card-body pb-2">
+            <div class="box-body">
+              <?php $attributes = array('name' => 'edit_user_photo', 'id' => 'edit_user_photo', 'autocomplete' => 'off');?>
+              <?php $hidden = array('token' => $segment_id);?>
+              <?= form_open('erp/employees/update_profile_photo', $attributes, $hidden);?>
+              <div class="form-body">
+                <div class="row">
+                  <div class="col-md-12">
+                    <div class="form-group">
+                      <label for="logo">
+                        <?= lang('Main.xin_e_details_profile_picture');?>
+                        <span class="text-danger">*</span> </label>
+                      <div class="custom-file">
+                        <input type="file" class="custom-file-input" name="file">
+                        <label class="custom-file-label">
+                          <?= lang('Main.xin_choose_file');?>
+                        </label>
+                        <small>
+                        <?= lang('Main.xin_company_file_type');?>
+                        </small> </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
-          
+          <div class="card-footer text-right">
+            <button  type="submit" class="btn btn-primary">
+            <?= lang('Employees.xin_update_pic');?>
+            </button>
+          </div>
+          <?= form_close(); ?>
         </div>
         </div>
     </div>
