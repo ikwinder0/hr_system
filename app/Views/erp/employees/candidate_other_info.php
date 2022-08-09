646eabba499c31bc4724258ba8b5c2b6c54533f7
ikwinder0 <ikwinder842@gmail.com>
2022-08-09 15:59:34 +0530

    changes

--- app/Views/erp/employees/candidate_other_info.php
+++ app/Views/erp/employees/candidate_other_info.php
@@ -96,60 +96,185 @@ $interview = $JobinterviewsModel->where('candidate_id', $result['user_id'])->fir
 
 <div class="row"> 
   <!-- [] start -->
-  <div class="col-lg-4">
-    <div class="card user-card user-card-1">
-      <div class="card-body pb-0">
-        <div class="float-right">
-          <?= $_status?>
-        </div>
-        <div class="media user-about-block align-items-center mt-0 mb-3">
-          <div class="position-relative d-inline-block"> <img class="img-radius img-fluid wid-80" src="<?= base_url().'/public/uploads/users/'.$result['profile_photo'];?>" alt="<?= $result['first_name'].' '.$result['last_name']; ?>">
-		   <?php if($result['is_active']=='1'): ?>
-            <div class="certificated-badge"> <i class="fas fa-certificate text-primary bg-icon"></i> <i class="fas fa-check front-icon text-white"></i> </div>
-			<?php endif; ?>
-          </div>
-          <div class="media-body ml-3">
-            <h6 class="mb-1">
-              <?= $result['first_name'].' '.$result['last_name']; ?>
-            </h6>
-            <p class="mb-0 text-muted">
-              <?= $idesignations['designation_name'];?>
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
   <input type="hidden" id="user_id" value="<?= udecode($segment_id);?>" />
-  <div class="col-lg-8">
+  <div class="col-lg-12">
     <div class="tab-content" id="user-set-tabContent">
         <div class="tab-pane fade show active" id="user-set-basicinfo" role="tabpanel" aria-labelledby="user-set-basicinfo-tab">
         <div class="card">
           <div class="card-header">
-            <h5><i data-feather="image" class="icon-svg-primary wid-20"></i><span class="p-l-5">
-              <?= lang('Main.xin_e_details_profile_picture');?>
+            <h5><i data-feather="file-text" class="icon-svg-primary wid-20"></i><span class="p-l-5">
+              <?= lang('Main.xin_employee_basic_title');?>
               </span></h5>
           </div>
           <div class="card-body pb-2">
             <div class="box-body">
-              <?php $attributes = array('name' => 'edit_user_photo', 'id' => 'edit_user_photo', 'autocomplete' => 'off');?>
+              <?php $attributes = array('name' => 'edit_user', 'id' => 'edit_user', 'autocomplete' => 'off');?>
               <?php $hidden = array('token' => $segment_id);?>
-              <?= form_open('erp/employees/update_profile_photo', $attributes, $hidden);?>
+              <?= form_open('erp/employees/update_basic_info', $attributes, $hidden);?>
               <div class="form-body">
+			    <div class="row">
+                  <div class="col-md-6">
+				    <div class="form-group">
+						<label for="logo">
+							<?= lang('Main.xin_position_applied_for');?>
+							<span class="text-danger">*</span>
+						</label>
+						<div class="input-group">
+							<select
+								class="form-control"
+								name="applied_for"
+								data-plugin="select_hrm">
+								<option value="">
+									<?= lang('Employees.xin_select');?>
+								</option>
+								<?php foreach($designations as $job){  ?>
+								<option value="<?= $job['designation_id']; ?>" <?= ($job['designation_id'] == $employee_detail['designation_id']) ? 'selected':''; ?> >
+									<?= $job['designation_name']; ?>
+								</option>
+								<?php }  ?>
+							</select>
+						</div>
+						</div>
+					</div>
+				</div>
+			
                 <div class="row">
-                  <div class="col-md-12">
+                  <div class="col-md-6">
                     <div class="form-group">
-                      <label for="logo">
-                        <?= lang('Main.xin_e_details_profile_picture');?>
+                      <label for="company_name">
+                        <?= lang('Main.xin_employee_first_name');?>
                         <span class="text-danger">*</span> </label>
-                      <div class="custom-file">
-                        <input type="file" class="custom-file-input" name="file">
-                        <label class="custom-file-label">
-                          <?= lang('Main.xin_choose_file');?>
-                        </label>
-                        <small>
-                        <?= lang('Main.xin_company_file_type');?>
-                        </small> </div>
+                      <div class="input-group">
+                        <div class="input-group-prepend"><span class="input-group-text"><i class="fas fa-user"></i></span></div>
+                        <input class="form-control" placeholder="<?= lang('Main.xin_employee_first_name');?>" name="first_name" type="text" value="<?= $result['first_name'];?>">
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-md-6">
+                    <div class="form-group">
+                      <label for="last_name" class="control-label">
+                        <?= lang('Main.xin_employee_last_name');?>
+                        <span class="text-danger">*</span></label>
+                      <div class="input-group">
+                        <div class="input-group-prepend"><span class="input-group-text"><i class="fas fa-user"></i></span></div>
+                        <input class="form-control" placeholder="<?= lang('Main.xin_employee_last_name');?>" name="last_name" type="text" value="<?= $result['last_name'];?>">
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-md-6">
+                    <div class="form-group">
+                      <label for="contact_number">
+                        <?= lang('Main.xin_contact_number');?>
+                        <span class="text-danger">*</span></label>
+                      <input class="form-control" placeholder="<?= lang('Main.xin_contact_number');?>" name="contact_number" type="text" value="<?= $result['contact_number'];?>">
+                    </div>
+                  </div>
+                  <div class="col-md-6">
+                    <div class="form-group">
+                      <label for="gender" class="control-label">
+                        <?= lang('Main.xin_employee_gender');?>
+                      </label>
+                      <select class="form-control" name="gender" data-plugin="select_hrm" data-placeholder="<?= lang('Main.xin_employee_gender');?>">
+                        <option value="1" <?php if($result['gender']==1):?> selected="selected"<?php endif;?>>
+                        <?= lang('Main.xin_gender_male');?>
+                        </option>
+                        <option value="2" <?php if($result['gender']==2):?> selected="selected"<?php endif;?>>
+                        <?= lang('Main.xin_gender_female');?>
+                        </option>
+                      </select>
+                    </div>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-md-6">
+                    <div class="form-group">
+                      <label for="date_of_birth">
+                        <?= lang('Employees.xin_employee_dob');?>
+                      </label>
+                      <div class="input-group">
+                        <input class="form-control date" placeholder="<?= lang('Employees.xin_employee_dob');?>" name="date_of_birth" type="text" value="<?= $employee_detail['date_of_birth'];?>">
+                        <div class="input-group-append"><span class="input-group-text"><i class="fas fa-calendar-alt"></i></span></div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-md-6">
+                    <div class="form-group">
+                      <label for="marital_status" class="control-label">
+                        <?= lang('Employees.xin_employee_mstatus');?>
+                      </label>
+                      <select class="form-control" name="marital_status" data-plugin="select_hrm" data-placeholder="<?= lang('Employees.xin_employee_mstatus');?>">
+                        <option value="0" <?php if($employee_detail['marital_status']==0):?> selected <?php endif; ?>>
+                        <?= lang('Employees.xin_status_single');?>
+                        </option>
+                        <option value="1" <?php if($employee_detail['marital_status']==1):?> selected <?php endif; ?>>
+                        <?= lang('Employees.xin_status_married');?>
+                        </option>
+                        <option value="2" <?php if($employee_detail['marital_status']==2):?> selected <?php endif; ?>>
+                        <?= lang('Employees.xin_status_widowed');?>
+                        </option>
+                        <option value="3" <?php if($employee_detail['marital_status']==3):?> selected <?php endif; ?>>
+                        <?= lang('Employees.xin_status_divorced_separated');?>
+                        </option>
+                      </select>
+                    </div>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-md-4">
+                    <div class="form-group">
+                      <label for="estate">
+                        <?= lang('Main.xin_state');?>
+                      </label>
+                      <input class="form-control" placeholder="<?= lang('Main.xin_state');?>" name="state" type="text" value="<?= $result['state'];?>">
+                    </div>
+                  </div>
+                  <div class="col-md-4">
+                    <div class="form-group">
+                      <label for="ecity">
+                        <?= lang('Main.xin_city');?>
+                      </label>
+                      <input class="form-control" placeholder="<?= lang('Main.xin_city');?>" name="city" type="text" value="<?= $result['city'];?>">
+                    </div>
+                  </div>
+                  <div class="col-md-4">
+                    <div class="form-group">
+                      <label for="ezipcode" class="control-label">
+                        <?= lang('Main.xin_zipcode');?>
+                      </label>
+                      <input class="form-control" placeholder="<?= lang('Main.xin_zipcode');?>" name="zipcode" type="text" value="<?= $result['zipcode'];?>">
+                    </div>
+                  </div>
+                  <div class="col-md-6">
+                    <div class="form-group">
+                      <label for="email" class="control-label">
+                        <?= lang('Employees.xin_ethnicity_type_title');?>
+                      </label>
+                      <select class="form-control" name="religion" data-plugin="select_hrm" data-placeholder="<?= lang('Employees.xin_ethnicity_type_title');?>">
+                        <option value=""></option>
+                        <?php foreach($religion as $ireligion) {?>
+                        <option value="<?= $ireligion['constants_id']?>" <?php if($ireligion['constants_id']==$employee_detail['religion_id']):?> selected="selected"<?php endif;?>>
+                        <?= $ireligion['category_name']?>
+                        </option>
+                        <?php } ?>
+                      </select>
+                    </div>
+                  </div>
+             
+                  <div class="col-md-6">
+                    <div class="form-group">
+                      <label for="nationality_id">
+                        <?= lang('Employees.xin_nationality');?>
+                      </label>
+                      <select class="form-control" name="country" data-plugin="select_hrm" data-placeholder="<?= lang('Employees.xin_nationality');?>">
+                        <option value="">
+                        <?= lang('Main.xin_select_one');?>
+                        </option>
+                        <?php foreach($all_countries as $country) {?>
+                        <option value="<?= $country['country_id'];?>" <?php if($country['country_id'] == $result['country']):?> selected="selected"<?php endif;?>>
+                        <?= $country['country_name'];?>
+                        </option>
+                        <?php } ?>
+                      </select>
                     </div>
                   </div>
                 </div>
@@ -158,7 +283,7 @@ $interview = $JobinterviewsModel->where('candidate_id', $result['user_id'])->fir
           </div>
           <div class="card-footer text-right">
             <button  type="submit" class="btn btn-primary">
-            <?= lang('Employees.xin_update_pic');?>
+            <?= lang('Employees.xin_update_profile');?>
             </button>
           </div>
           <?= form_close(); ?>
