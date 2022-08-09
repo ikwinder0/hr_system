3b7c6a00ed14c3ba088a46a93c1d096543673453
ikwinder0 <ikwinder842@gmail.com>
2022-08-09 16:02:05 +0530

    changes

--- app/Views/erp/employees/candidate_other_info.php
+++ app/Views/erp/employees/candidate_other_info.php
@@ -103,7 +103,7 @@ $interview = $JobinterviewsModel->where('candidate_id', $result['user_id'])->fir
       </a> </li>
     <?php } ?>
 	<?php if(in_array('shift1',staff_role_resource()) || $user_info['user_type'] == 'company') { ?>
-    <li class="nav-item clickable"> <a href="<?= site_url('erp/candidate-other-info/'.$segment_id);?>" class="mb-3 nav-link"> <span class="sw-done-icon feather icon-check-circle"></span> <span class="sw-icon fas fa-info"></span>
+    <li class="nav-item active"> <a href="<?= site_url('erp/candidate-other-info/'.$segment_id);?>" class="mb-3 nav-link"> <span class="sw-done-icon feather icon-check-circle"></span> <span class="sw-icon fas fa-info"></span>
       Other Info
       <div class="text-muted small">
         Set up other info
