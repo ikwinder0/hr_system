6e3cc7b66521978fef67852755b23819897b5feb
ikwinder0 <ikwinder842@gmail.com>
2022-08-10 11:40:09 +0530

    ch

--- app/Views/erp/employees/view_application.php
+++ app/Views/erp/employees/view_application.php
@@ -391,7 +391,6 @@ $interview = $JobinterviewsModel->where('candidate_id', $result['user_id'])->fir
 				</div>
 			</div>
 		</div>
-    </div>
 	<div class="row">
         <div class="col-md-12">
 			<div class="card user-profile-list">
@@ -456,4 +455,6 @@ $interview = $JobinterviewsModel->where('candidate_id', $result['user_id'])->fir
 		</div>
 		
 	<?php } ?>
+	
+</div>
   <!-- [] end --> 
