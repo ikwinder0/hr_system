47239f7c3ee35571a304b8c9fc55e6875e92ab66
ikwinder0 <ikwinder842@gmail.com>
2022-08-10 09:58:46 +0530

    ch

--- app/Views/erp/employees/view_application.php
+++ app/Views/erp/employees/view_application.php
@@ -148,6 +148,27 @@ $interview = $JobinterviewsModel->where('candidate_id', $result['user_id'])->fir
 				</div>
 			</div>
 		</div>
+		<div class="col-md-12">
+			<div class="card">
+				<div class="card-body p-0">
+					<div class="row">
+						<div class="col-md-4">
+							<h3>Name in English</h3>
+						</div>
+						<div class="col-md-4">
+							<h3>Name in Arabic</h3>
+							 
+							
+						</div>
+						<div class="col-md-4">
+							<h3>Preferred Name</h3>
+							  
+							
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
         <div class="col-md-12">
         <div class="card">
           <div class="card-body p-0">
