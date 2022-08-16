f3e36ab581ec2cb6fb7068129270ff231587a9b7
ikwinder0 <ikwinder842@gmail.com>
2022-08-16 20:06:39 +0530

    ch

--- app/Views/erp/employees/staff_list.php
+++ app/Views/erp/employees/staff_list.php
@@ -84,7 +84,7 @@ $get_animate='';
 							<h5 class="modal-title" id="exampleModalLabel">Import Candidate</h5>
 							<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 							  <span aria-hidden="true">&times;</span>
-							</button>
+							</button> 
 						  </div>
 						  <form>
 						  <div class="modal-body">
