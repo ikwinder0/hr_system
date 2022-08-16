02cc23d397f60afb7a0237653da84085d6917e20
ikwinder0 <ikwinder842@gmail.com>
2022-08-16 18:58:22 +0530

    ch

--- app/Models/StaffdetailsModel.php
+++ app/Models/StaffdetailsModel.php
@@ -10,7 +10,7 @@ class StaffdetailsModel extends Model {
     protected $primaryKey = 'staff_details_id';
     
 	// get all fields of employees details table
-    protected $allowedFields = ['staff_details_id','user_id','employee_id','department_id','designation_id','office_shift_id','basic_salary','hourly_rate','salay_type','leave_categories','role_description','date_of_joining','date_of_leaving','date_of_birth','marital_status','religion_id','blood_group','citizenship_id','bio','experience','fb_profile','twitter_profile','gplus_profile','linkedin_profile','account_title','account_number','bank_name','iban','swift_code','bank_branch','contact_full_name','contact_phone_no','experience_1','experience_2','experience_3','high_school','degree','other_education','contact_email','contact_address','created_at','home_country_address','emergency_contact_name','emergency_contact_number','preferred_language'];
+    protected $allowedFields = ['staff_details_id','user_id','gen_id','employee_id','department_id','designation_id','office_shift_id','basic_salary','hourly_rate','salay_type','leave_categories','role_description','date_of_joining','date_of_leaving','date_of_birth','marital_status','religion_id','blood_group','citizenship_id','bio','experience','fb_profile','twitter_profile','gplus_profile','linkedin_profile','account_title','account_number','bank_name','iban','swift_code','bank_branch','contact_full_name','contact_phone_no','experience_1','experience_2','experience_3','high_school','degree','other_education','contact_email','contact_address','created_at','home_country_address','emergency_contact_name','emergency_contact_number','preferred_language'];
 	
 	protected $validationRules = [];
 	protected $validationMessages = [];
